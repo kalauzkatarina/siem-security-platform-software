80e6e2b3d9c754a51d7f86c7f16f2af29a7d8850
Danijel Jovanović <danijel.xda@gmail.com>
2025-11-21 15:07:58 +0100

    Delete infrastructure/microservices/auth-microservice directory

--- infrastructure/microservices/auth-microservice/src/Domain/services/IAuthService.ts
+++
@@ -1,8 +0,0 @@
-import { LoginUserDTO } from "../DTOs/LoginUserDTO";
-import { RegistrationUserDTO } from "../DTOs/RegistrationUserDTO";
-import { AuthResponseType } from "../types/AuthResponse";
-
-export interface IAuthService {
-  login(data: LoginUserDTO): Promise<AuthResponseType>;
-  register(data: RegistrationUserDTO): Promise<AuthResponseType>;
-}
