cd3d339422016089bc1300af43985a7d0744f934
usernameborko <radovic.borko21@gmail.com>
2025-11-29 20:33:26 +0100

    [SIEM-AUTH]: Implementing a small method for validating user role.

--- infrastructure/microservices/siem-auth-microservice/src/Services/ValidationService.ts
+++ infrastructure/microservices/siem-auth-microservice/src/Services/ValidationService.ts
@@ -2,7 +2,19 @@ import { IValidationService } from '../Domain/services/IValidationService';
 import { AuthTokenClaims } from '../Domain/types/AuthTokenClaims';
 import { VerifyResult } from '../Domain/types/VerifyResult';
 
+export type JWTValidationOptions = {
+    secret?: string;
+    sysAdminRoleId?: number;
+}
+
 export class ValidationService implements IValidationService {
+    private secret: string;
+    private sysAdminRoleId: number;
+
+    constructor(secret: string, sysAdminRoleId?: number) {
+        this.secret = secret;
+        this.sysAdminRoleId = 1;
+    }
 
     normalizeAuthHeader(header?: string): string | null {
         if(!header){
@@ -21,8 +33,22 @@ export class ValidationService implements IValidationService {
     verifyToken(token: string): Promise<VerifyResult> {
         throw new Error('Method not implemented.');
     }
+
     isSysAdminPayload(payload?: AuthTokenClaims, sysAdminRoleId?: number): boolean {
-        throw new Error('Method not implemented.');
+       if(!payload){
+        return false;
+       }
+
+       let sysId = this.sysAdminRoleId;
+       if(sysAdminRoleId !== undefined && sysAdminRoleId !== null){
+        const parsed = Number(sysAdminRoleId);
+
+        if(!Number.isNaN(parsed)){
+            sysId = parsed;
+        }
+       }
+
+       return payload.role === sysId;
     }
 
 }
