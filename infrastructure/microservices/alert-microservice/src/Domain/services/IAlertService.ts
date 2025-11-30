a355602eb94ff0e5a6860cb28a02004ba51f62cb
Ivana Lazović <lazovic.pr93.2022@gmail.com>
2025-11-30 10:55:08 +0100

    Updated IAlertService

--- infrastructure/microservices/alert-microservice/src/Domain/services/IAlertService.ts
+++ infrastructure/microservices/alert-microservice/src/Domain/services/IAlertService.ts
@@ -1,32 +1,19 @@
-import { AlertDTO, CreateAlertDTO, ResolveAlertDTO } from "../DTOs/AlertDTO";
+import { AlertDTO } from "../DTOs/AlertDTO";
+import { CreateAlertDTO } from "../DTOs/CreateAlertDTO";
+import { ResolveAlertDTO } from "../DTOs/ResolveAlertDTO";
 import { AlertSeverity } from "../enums/AlertSeverity";
 import { AlertStatus } from "../enums/AlertStatus";
 
 export interface IAlertService {
-  // Kreiranje alerta
   createAlert(data: CreateAlertDTO): Promise<AlertDTO>;
-  
-  // Pregled svih alerta
   getAllAlerts(): Promise<AlertDTO[]>;
-  
-  // Pregled jednog alerta
   getAlertById(id: number): Promise<AlertDTO>;
-  
-  // Pregled po severity-ju
   getAlertsBySeverity(severity: AlertSeverity): Promise<AlertDTO[]>;
-  
-  // Pregled po statusu
   getAlertsByStatus(status: AlertStatus): Promise<AlertDTO[]>;
-  
-  // Rješavanje alerta
   resolveAlert(id: number, data: ResolveAlertDTO): Promise<AlertDTO>;
-  
-  // Ažuriranje statusa
   updateAlertStatus(id: number, status: AlertStatus): Promise<AlertDTO>;
-  
-  // Brisanje alerta
   deleteAlert(id: number): Promise<boolean>;
-  
-  // Glavna funkcija - analiza događaja i kreiranje alerta ako ima prijetnju
-  analyzeEventsForThreats(eventIds: number[]): Promise<AlertDTO | null>;
+
+  // poziva ga AnalysisEngine
+  showAlert(correlationId: number): Promise<void>;
 }
