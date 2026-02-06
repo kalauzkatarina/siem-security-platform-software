de7b08f001a2c61dad0bed749019ae3e6a26f143
Dajana Bulic <bulic.dajanaaa@gmail.com>
2026-02-06 16:36:07 +0100

    merge

--- infrastructure/gateway-api/src/WebAPI/controllers/IntegrityGatewayController.ts
+++ infrastructure/gateway-api/src/WebAPI/controllers/IntegrityGatewayController.ts
@@ -9,11 +9,7 @@ export class IntegrityGatewayController {
     constructor(
         private readonly gatewayService: IGatewayService,
         private readonly authenticate: any,
-<<<<<<< HEAD
         private readonly loggerService: ILogerService
-=======
-        private readonly logger: ILogerService
->>>>>>> f51f5f8e12bc7d5fbaf8bd0fb9c11011a3a01abe
     ) {
         this.router = Router();
         this.initializeRoutes();
@@ -26,7 +22,6 @@ export class IntegrityGatewayController {
             this.initializeHashChain.bind(this)
         );
 
-<<<<<<< HEAD
         this.router.get(
             "/integrity/verify",
             /* this.authenticate, */
@@ -41,58 +36,6 @@ export class IntegrityGatewayController {
     }
 
     private async initializeHashChain(req: Request, res: Response): Promise<void> {
-=======
-        this.router.post("/", this.proxyRequest.bind(this));
-        this.router.post("/integrity/status",
-            this.authenticate,
-            requireSysAdmin,
-            this.getStatus.bind(this));
-
-        this.router.get("/integrity/compromised",
-            this.authenticate,
-            requireSysAdmin,
-            this.getCompromised.bind(this));
-
-        this.router.post("/integrity/verify",
-            this.authenticate,
-            requireSysAdmin,
-            this.verify.bind(this));
-    }
-
-    private async proxyRequest(req: Request, res: Response): Promise<void> {
-        const { url, method, data, headers } = req.body;
-
-        if (url === "integrity/status") {
-            return this.getStatus(req, res);
-        }
-
-        if (url === "integrity/verify") {
-            return this.verify(req, res);
-        }
-
-        res.status(404).json({ message: `Integrity Proxy: Route ${url} not found` });
-    }
-
-    private async getStatus(req: Request, res: Response): Promise<void> {
-        console.log("Pozvan POST /integrity/status");
-
-        try {
-            const data = await this.gatewayService.getIntegrityStatus();
-
-
-            res.json({
-                success: true,
-                response: data
-            });
-
-        } catch (err) {
-            console.error("Greška u getStatus kontroleru:", err);
-            await this.handleError(err, res);
-        }
-    }
-
-    private async getCompromised(req: Request, res: Response): Promise<void> {
->>>>>>> f51f5f8e12bc7d5fbaf8bd0fb9c11011a3a01abe
         try {
             const result = await this.gatewayService.initializeHashChain();
             res.status(200).json(result);
